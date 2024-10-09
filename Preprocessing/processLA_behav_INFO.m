c35f03e4561ce2b05af74d44b56a4aa31c1aab73
lisamhirt <lisa.hirt@cuanschutz.edu>
2024-10-09 09:20:47 -0600

    Initial file transfer

---
+++ Preprocessing/processLA_behav_INFO.m
@@ -0,0 +1,16 @@
+%% Original code from JAT
+% subID = 'clase018';
+% nwbDi = 'D:\LossAversion\Patient folders\CLASE018\NWB-processing\NWB_Data';
+% nwbN = 'CLASE018_Session_3_filter.nwb';
+% behD = 'D:\LossAversion\Patient folders\CLASE018\Behavioral-data\EventBehavior';
+% ttlID = 'XXXXX';
+% ttlS = 1; % instead of 1 put 3 
+% processLA_behav_v3(subID , ttlS ,ttlID , nwbDi, nwbN , behD)
+%%
+subID = 'CLASE026';
+nwbDi = 'Y:\LossAversion\Patient folders\CLASE026\NWB-processing\2023-11-03_12-16-35\NWB_Data';
+nwbN = 'MW26_s2_Session_1_filter.nwb';
+behD = 'Y:\LossAversion\Patient folders\CLASE026\Behavioral-data\EventBehavior';
+ttlID = 'XXXXX';
+ttlS = 3; % instead of 1 put 3 
+processLA_behav_v3(subID , ttlS ,ttlID , nwbDi, nwbN , behD)
